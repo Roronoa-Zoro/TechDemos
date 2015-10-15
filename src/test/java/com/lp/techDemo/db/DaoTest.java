9a522fff7287bf03490d6e2ede922890fa929a94
lipeng <pengli51@creditease.cn>
2015-10-15 09:46:56 +0800

    update junit version, add spring test

--- src/test/java/com/lp/techDemo/db/DaoTest.java
+++ src/test/java/com/lp/techDemo/db/DaoTest.java
@@ -2,16 +2,19 @@ package com.lp.techDemo.db;
 
 import static org.junit.Assert.*;
 
+import javax.annotation.Resource;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
+import com.lp.techDemo.AstractTestCase;
 import com.lp.techDemo.db.dao.EmployeeDao;
 import com.lp.techDemo.db.model.Employees;
 
-public class DaoTest {
+@ContextConfiguration(locations={"classpath:mybatis-application.xml"})
+public class DaoTest extends AstractTestCase{
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,11 +23,11 @@ public class DaoTest {
 	@Before
 	public void setUp() throws Exception {
 	}
+	@Resource
+	private EmployeeDao dao;
 
 	@Test
 	public void test() {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("mybatis-application.xml");
-		EmployeeDao dao = (EmployeeDao) ac.getBean("employeeDao");
 		Employees e = dao.getEmployee(100);
 		assertNotNull(e);
 		System.err.println(e);
